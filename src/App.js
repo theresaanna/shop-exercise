bb9546f2faf53dda20ae16069b299e023f724461
Theresa <theresasumma@gmail.com>
2021-08-27 18:36:00 -0700

    implement function-based components and implement cart functionality

--- src/App.js
+++ src/App.js
@@ -1,53 +1,80 @@
-import React, {Component} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import ProductList from "./components/product-list";
 import Cart from "./components/cart";
 import 'h8k-components';
 
-const title = "HackerShop";
+function configProducts(products) {
+  // TODO? can we store these values in the data?
+  products.map((product, index) => {
+      product.id = index + 1;
+      product.image = `/images/items/${product.name.toLocaleLowerCase()}.png`;
+      product.cartQuantity = 0;
+      return product;
+  });
+  return products;
+}
 
-class App extends Component {
-    constructor() {
-        super();
-        const products = [...PRODUCTS].map((product, index) => {
-            product.id = index + 1;
-            product.image = `/images/items/${product.name.toLocaleLowerCase()}.png`;
-            product.cartQuantity = 0;
-            return product;
-        });
-        this.state = {
-            cart: {
-                items: [],
-                subTotal: 0,
-                totalPrice: 0,
-                discount: 0,
-                selectedCoupon: '0'
-            },
-            products
-        }
-        this.addToCart = this.addToCart.bind(this);
-        this.removeFromCart = this.removeFromCart.bind(this);
+function App () {
+  const title = "HackerShop";
+  const [products, setProducts] = useState(() => configProducts(PRODUCTS));
+  const [cartItems, setCartItems] = useState([]);
+  const [cartSubtotal, setCartSubtotal] = useState(0);
+  const [cartTotalPrice, setCartTotalPrice] = useState(0);
+  const [cartDiscount, setCartDiscount] = useState(0);
+  const [cartCoupon, setCartCoupon] = useState('0');
+
+  useEffect(() => {
+    document.title = title;
+  });
+
+  useEffect(() => {
+    if (cartItems.length > 0) {
+      const reducer = (accumulator, currentValue) => accumulator + currentValue;
+      const prices = cartItems.map(item => item.price * item.cartQuantity);
+      setCartSubtotal(prices.reduce(reducer));
     }
+  },
+    [cartItems]
+  );
 
-    addToCart(index) {
-    }
+  useEffect(() => {
+    setCartDiscount((cartCoupon / 100) * cartSubtotal);
+  },
+    [cartCoupon, cartSubtotal]
+  );
 
-    removeFromCart(index) {
-    }
+  useEffect(() => {
+    setCartTotalPrice(cartSubtotal - cartDiscount)
+  },
+    [cartSubtotal, cartCoupon, cartTotalPrice, cartDiscount]
+  );
 
+  const couponHandler = (discount) => {
+    setCartCoupon(discount);
+    setCartDiscount((discount / 100) * cartSubtotal);
+  };
 
-    render() {
-
-        return (
-            <div>
-                <h8k-navbar header={title}></h8k-navbar>
-                <div className="layout-row shop-component">
-                    <ProductList products={this.state.products}/>
-                    <Cart cart={this.state.cart}/>
-                </div>
-            </div>
-        );
-    }
+  return (
+      <div>
+          <h8k-navbar header={title}></h8k-navbar>
+          <div className="layout-row shop-component">
+              <ProductList
+                products={products}
+                cartItems={cartItems}
+                setCartItems={setCartItems}
+                cartSubtotal={cartSubtotal}
+                setCartSubtotal={setCartSubtotal}/>
+              <Cart
+                cartItems={cartItems}
+                cartSubtotal={cartSubtotal}
+                cartTotalPrice={cartTotalPrice}
+                cartDiscount={cartDiscount}
+                cartCoupon={cartCoupon}
+                couponHandler={couponHandler}/>
+          </div>
+      </div>
+  );
 }
 
 export const PRODUCTS = [
--- src/components/product-list/index.js
+++ src/components/product-list/index.js
@@ -1,44 +1,57 @@
-import React, {Component} from "react";
+import React from "react";
 import "./index.css";
 
-export default class ProductList extends Component {
-    constructor() {
-        super();
-    }
-
-    render() {
-
-        return (
-            <div className="layout-row wrap justify-content-center flex-70 app-product-list">
-                {this.props.products.map((product, i) => {
-                    return (
-                        <section className="w-30"
-                                 data-testid={'product-item-' + i}
-                                 key={product.id}>
-                            <div className="card ma-16">
-                                <img alt="Your Cart" src={product.image}
-                                     className="d-inline-block align-top product-image"/>
-                                <div className="card-text pa-4">
-                                    <h5 className="ma-0 text-center">{product.name}</h5>
-                                    <p className="ma-0 mt-8 text-center">${product.price}</p>
-                                </div>
-                                <div className="card-actions justify-content-center pa-4">
-                                    <button className="x-small outlined" data-testid="btn-item-add">
-                                        Add To Cart
-                                    </button>
-
-                                    <button className="x-small danger" data-testid="btn-item-remove">
-                                        Remove
-                                    </button>
-                                </div>
+export default function ProductList({products,
+                                    cartItems,
+                                    setCartItems,
+                                    cartSubtotal,
+                                    setCartSubtotal}) {
+    // TODO? should this be broken out into two Components:
+    // ProductList and a Product component?
+    return (
+        <div className="layout-row wrap justify-content-center flex-70 app-product-list">
+            {products.map((product, i) => {
+                return (
+                    <section className="w-30"
+                             data-testid={'product-item-' + i}
+                             key={product.id}>
+                        <div className="card ma-16">
+                            <img alt="Your Cart" src={product.image}
+                                 className="d-inline-block align-top product-image"/>
+                            <div className="card-text pa-4">
+                                <h5 className="ma-0 text-center">{product.name}</h5>
+                                <p className="ma-0 mt-8 text-center">${product.price}</p>
                             </div>
-                        </section>
-                    )
-                })}
+                            <div className="card-actions justify-content-center pa-4">
+                                <button className="x-small outlined"
+                                        data-testid="btn-item-add"
+                                        onClick={() => {
+                                            product.cartQuantity = product.cartQuantity + 1;
+                                            if (!cartItems.includes(product)) {
+                                              setCartItems([...cartItems, product])
+                                            }
+                                            else {
+                                              setCartItems([...cartItems])
+                                            }
+                                        }}>
+                                    Add To Cart
+                                </button>
 
-            </div>
+                                <button className="x-small danger"
+                                        data-testid="btn-item-remove"
+                                        onClick={() => {
+                                          setCartItems(cartItems.filter(item => item.id !== product.id))
+                                          setCartSubtotal(cartSubtotal - product.price * product.cartQuantity);
+                                          product.cartQuantity = 0;
+                                        }}>
+                                    Remove
+                                </button>
+                            </div>
+                        </div>
+                    </section>
+                )
+            })}
+        </div>
 
-        );
-    }
+    );
 }
-
